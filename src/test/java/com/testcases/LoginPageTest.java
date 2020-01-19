a21922436e6a49cc62b83352ff6dfacfed85b45c
kanat17990 <kussenov89@gmail.com>
2020-01-19 10:29:26 -0500

    second commit

--- src/test/java/com/testcases/LoginPageTest.java
+++ src/test/java/com/testcases/LoginPageTest.java
@@ -1,5 +1,35 @@
 package com.testcases;
 
-public class LoginPageTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-}
+import com.hrms.pages.LoginPage;
+import com.hrms.utils.CommonMethods;
+
+public class LoginPageTest extends CommonMethods {
+
+	//Test case to validate valid login
+		@Test
+		public void login() {
+			LoginPage login=new LoginPage();
+			sendText(login.username, "Admin");
+			sendText(login.password, "Hum@nhrm123");
+			Click(login.loginBtn);
+		}
+
+		/* Navigate to HRMS 
+		 * Enter uid
+		 * leave password field blank
+		 * click login
+		 * verify "Password cannot be empty" text 
+		 * close browser
+		 */
+		//@Test
+		public void negativeLogin() {
+			LoginPage login=new LoginPage();
+			sendText(login.username, "Admin");
+			Click(login.loginBtn);
+			String expected="Password cannot be empty";
+			//Assert.assertEquals(login.errMsg.getText(), expected);
+		}
+	}		
